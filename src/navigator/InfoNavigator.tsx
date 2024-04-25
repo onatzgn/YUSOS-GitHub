471109eec5bfa1f4bc97f947c62f717cb56a6c96
Monochrome <yigitcanmenengic@gmail.com>
2024-04-25 14:20:51 +0300

    infoScreen6362

--- src/navigator/InfoNavigator.tsx
+++ src/navigator/InfoNavigator.tsx
@@ -9,7 +9,7 @@ const InfoNavigator = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen
-                name="Info3333"
+                name="Info6362"
                 component={InfoScreen}
             />
         </Stack.Navigator>
