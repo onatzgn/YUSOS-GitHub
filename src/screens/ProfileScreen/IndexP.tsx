0f75a37e73e820d48b95489af12eca0ee620db6d
AltanTamer <emrealtan2002@gmail.com>
2024-04-25 15:47:31 +0300

    altan profil deneme4

--- src/screens/ProfileScreen/IndexP.tsx
+++ src/screens/ProfileScreen/IndexP.tsx
@@ -20,27 +20,45 @@ const ProfileScreen = () => {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
+    <ScrollView style={styles.scrollView} contentContainerStyle={styles.container}>
       <View>
         <Text> </Text>
         <View style={styles.profileContainer}>
-          <TouchableOpacity onPress={changeProfileImage}>
-            <Image source={userInfo.profileImage} style={styles.profileImage} />
-            {/* Profil resminin sağ alt köşesine tıklama işlevi buraya eklenebilir */}
+          <Image source={userInfo.profileImage} style={styles.profileImage} />
+          {/* "Change Profile Picture" butonu */}
+          <TouchableOpacity onPress={changeProfileImage} style={styles.changeProfileButton}>
+            <Text style={styles.plusSign}>+</Text>
           </TouchableOpacity>
           <Text style={styles.name}>{userInfo.name}</Text>
-          <TouchableOpacity onPress={changeUserInfo} style={styles.editProfileButton}>
-            <Text style={styles.editProfileText}>Edit Profile</Text>
-          </TouchableOpacity>
         </View>
         <View style={styles.infoContainer}>
-          <Text style={styles.infoHeading}>User Information</Text>
+          <View style={styles.infoHeaderContainer}>
+            <Text style={styles.infoHeading}>Bilgiler</Text>
+            {/* "Edit Profile" butonu */}
+            <TouchableOpacity onPress={changeUserInfo} style={styles.editInfoButton}>
+              <Text style={styles.plusSign}>+</Text>
+            </TouchableOpacity>
+          </View>
           {/* Kullanıcı bilgilerinin gösterileceği kutu buraya eklenebilir */}
+          <Text>
+            Kan Grubu: {"\n\n"}
+            Aile İrtibat No: {"\n\n"}
+            Sağlık Sorunları: {"\n\n"}
+            Adres: {"\n\n"}
+            Diğer:
+          </Text>
         </View>
         <View style={styles.activitiesContainer}>
-          <Text style={styles.activitiesHeading}>Past Activities</Text>
+          <Text style={styles.activitiesHeading}>Geçmiş Faaliyetler</Text>
           {/* Kullanıcının katıldığı geçmiş faaliyetlerin gösterileceği kutu buraya eklenebilir */}
-          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. {"\n"}
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.{"\n\n"}
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.{"\n"}
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.{"\n"}
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.{"\n"}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Text>
         </View>
       </View>
     </ScrollView>
