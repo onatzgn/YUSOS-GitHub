1f6aa8f82ff090fa893d183f012dda8934680096
Monochrome <yigitcanmenengic@gmail.com>
2024-04-26 19:46:11 +0300

    onatim alayim

--- src/screens/Homescreen/IndexH.tsx
+++ src/screens/Homescreen/IndexH.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View,Text } from 'react-native';
 import HeaderMain from '../../components/HeaderMain/IndexHeader';
 import Duyurular from '../../components/Duyurular';
 import styles from './styles';
@@ -9,6 +9,7 @@ function Homescreen() {
     <View style={styles.container}>
       <HeaderMain />
       <Duyurular />
+      
     </View>
   );
 }
